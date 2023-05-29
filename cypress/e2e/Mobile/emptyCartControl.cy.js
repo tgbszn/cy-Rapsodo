0e0db3238b47a00d3c413765069e38f2c51fd43f
tugbaasezenn <124561815+tugbaasezenn@users.noreply.github.com>
2023-05-29 20:05:14 +0300

    Case 2: The user clicks on the “Cart” icon at the top right area
    - The user verifies that shopping cart empty at that moment.

--- cypress/e2e/Mobile/emptyCartControl.cy.js
+++ cypress/e2e/Mobile/emptyCartControl.cy.js
@@ -7,7 +7,7 @@ import { EmptyCartControl } from "../../support/exportEmptyCartControl";
 browserify();
 
 describe('Case-2: The user clicks on the “Cart” icon at the top right area.', { testIsolation: false }, function () {
-    testWrapper({ itTitle: "The user verifies that shopping cart empty at that moment.", viewportGroup: "web" }, () => {
+    testWrapper({ itTitle: "The user verifies that shopping cart empty at that moment.", viewportGroup: "app" }, () => {
 
         EmptyCartControl()
 
